1e1190ae78a103e87bed49a3a53abd0ff41d4d7a
Lee  Ashmore <JLeeAshmore@gmail.com>
2020-02-16 13:08:13 -0700

    implemented the most basic prototype of the tty interface

--- src/tty/mod.rs
+++ src/tty/mod.rs
@@ -1,21 +1,62 @@
 use std::fs::File;
-use std::process::Child;
-use nix::pty::openpty
+use std::os::unix::io::FromRawFd;
+use std::process::{Child, Command, Stdio};
+
+use nix::pty::openpty;
+
+// Kill the program. For use if something goes wrong
+macro_rules! kill {
+    ($($arg:tt)*) => {
+        std::process::exit(1);
+    };
+}
+
+pub struct Shell {
+    program: String,
+}
+
+impl Shell {
+    pub fn new(program: &str) -> Shell {
+        let shell = Shell {
+            program: String::from(program),
+        };
+        shell
+    }
+}
 
 pub struct Pty {
     child: Child,
     file_descriptor: File,
 }
 
-pub fn new(winsize) -> Pty {
-    // create the file descriptors for master and slave
-        // the pty needs a window size to be created...
-    fd_ends = openpty(Some(winsize), None);
-    (master, slave) = fd_ends.master, fd_ends.slave;
+pub fn new() -> Pty {
+    // If there is a window for the process, then I need to pass in window size here
+    let fd = openpty(None, None).expect("Failed to open Pty");
 
-    // set the shell to comunicate with the slave side of the pty
+    let master = fd.master;
+    let slave = fd.slave;
+
+    // TODO: Once user configs are added, put this behind a if/else
+    let default_shell = Shell::new(&"/bin/bash");
+
+    //TODO: Add config/args for shell
+    let mut shell = Command::new(default_shell.program);
+    shell.stdin(unsafe { Stdio::from_raw_fd(slave) });
+    shell.stdout(unsafe { Stdio::from_raw_fd(slave) });
+    shell.stderr(unsafe { Stdio::from_raw_fd(slave) });
+
+    match shell.spawn() {
+        Ok(process) => {
+            let pty = Pty {
+                child: process,
+                file_descriptor: unsafe { File::from_raw_fd(master) },
+            };
+
+            return pty;
+        }
+        Err(err) => {
+            println!("The proccess could not be spawned: {}", err);
+            kill!();
+        }
+    };
 }
-
-// I want a class that will track all the input and output from a given process
-// The only process that this has to run, is the shell. Let the shell handle
-// everything else.
